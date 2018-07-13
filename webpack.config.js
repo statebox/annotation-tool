d808de8c4b37f74cb67cd69cef12d15fafd89e04
Jelle Herold <jelle@defekt.nl>
2018-07-13 12:48:34 +0200

    hak hak

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
     // mode: 'production',
     entry: path.join(__dirname, 'app', 'app.js'),
     output: {
-        path: path.join(__dirname, 'dist'),
+        path: path.join(__dirname, 'public'),
         filename: "bundle.js",
         library: 'Commenter',
         libraryTarget: 'var',
