d325f7a3e2226ba5814a6fa1dd1f264b0fa3f40a
Jelle Herold <jelle@defekt.nl>
2018-07-31 18:31:03 +0200

    download link

--- app/ui/revisionslist.js
+++ app/ui/revisionslist.js
@@ -2,6 +2,16 @@ const m = require('mithril')
 
 const State = require('../state.js')
 
+const { distanceInWordsToNow } = require('date-fns')
+
+// Takes an ISO time and returns a string representing how
+// long ago the date represents.
+function prettyDate(datestring) {
+    let s = 'published: ' + distanceInWordsToNow(new Date(datestring)) + ' ago'
+    console.log(s)
+    return s
+}
+
 var RevisionsList = {
     view: function (vnode) {
         const revisions = State.revisions().map(function (r) {
@@ -13,10 +23,12 @@ var RevisionsList = {
                     selectedComment: [0,0]
                 })
             }
-            return m("a.revision-list-item", attrs,
+            return m(".revision-list-item",
                 [
-                    m('span.timestamp', r.timestamp),
-                    m('code', r.revision)
+                    m('a.timestamp',  attrs, prettyDate(r.timestamp)),
+                    m('span', ['rev: ', m('a.revision', attrs, r.revision)]),
+                    m('a.to-comments', attrs, 'click to browse online and annotate'),
+                    m('a.download-pdf', {href: r.url}, 'download PDF')
                 ]
             )
         })
